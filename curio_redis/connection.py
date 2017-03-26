a8680791c7b0b4b7a1a3a5845e97c2b6ed76d88c
Living42 <qzeqing@gmail.com>
2017-03-26 20:09:25 +0800

    Drop sync methods

--- curio_redis/connection.py
+++ curio_redis/connection.py
@@ -13,28 +13,11 @@ class Connection:
         self._reader = hiredis.Reader(protocolError=errors.ProtocolError,
                                       replyError=errors.ReplyError)
 
-    def connect(self):
-        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        with self._sock.blocking() as sock:
-            sock.connect((self.host, self.port))
-        self._sock.setsockopt(socket.SOL_SOCKET, socket.TCP_NODELAY, 1)
-
-    @awaitable(connect)
     async def connect(self):
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         await self._sock.connect((self.host, self.port))
         self._sock.setsockopt(socket.SOL_SOCKET, socket.TCP_NODELAY, 1)
 
-    def close(self):
-        if self._sock is None:
-            return
-
-        with self._sock.blocking() as sock:
-            sock.close()
-
-        self._sock = None
-
-    @awaitable(close)
     async def close(self):
         if self._sock is None:
             return
@@ -42,19 +25,22 @@ class Connection:
         await self._sock.close()
         self._sock = None
 
-    def send_command(self, *args):
-        if self._sock is None:
-            self.connect()
-
-        with self._sock.blocking() as sock:
-            return sock.sendall(self.pack_command(*args))
-
-    @awaitable(send_command)
     async def send_command(self, *args):
         if self._sock is None:
             await self.connect()
 
         return await self._sock.sendall(self.pack_command(*args))
+
+    async def read_response(self):
+        if self._sock is None:
+            await self.connect()
+
+        while True:
+            data = await self._sock.recv(65536)
+            self._reader.feed(data)
+            response = self._reader.gets()
+            if response is not False:
+                return response
 
     def pack_command(self, *args):
         buf = bytearray()
@@ -73,26 +59,3 @@ class Connection:
 
         return buf
 
-    def read_response(self):
-        if self._sock is None:
-            self.connect()
-
-        with self._sock.blocking() as sock:
-            while True:
-                data = sock.recv(65536)
-                self._reader.feed(data)
-                response = self._reader.gets()
-                if response is not False:
-                    return response
-
-    @awaitable(read_response)
-    async def read_response(self):
-        if self._sock is None:
-            await self.connect()
-
-        while True:
-            data = await self._sock.recv(65536)
-            self._reader.feed(data)
-            response = self._reader.gets()
-            if response is not False:
-                return response
